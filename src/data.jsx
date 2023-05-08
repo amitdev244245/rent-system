1a6a8780052097ad2eafdcb92d441151e8e3c7c8
Amit Sharma <amitdev244245@gmail.com>
2023-05-09 01:28:45 +0530

    updated record april month

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
 const data = [
     {
         heading: "Room 1", name: "Vivek", mobile: "7011544086", record: [
-            { month: "Apr 2023", newUnits: "-", oldUnits: "85", rentAmt: 3500, status: "Unpaid" },
+            { month: "Apr 2023", newUnits: "180", oldUnits: "85", rentAmt: 3500, status: "Paid" },
             { month: "Mar 2023", newUnits: "85", oldUnits: "0", rentAmt: 3500, status: "Paid" },
             // { month: "Feb 2023", newUnits: "Avg", oldUnits: "Avg", rentAmt: 3500, status: "Paid" },
             // { month: "Jan 2023", newUnits: "2385", oldUnits: "2372", rentAmt: 3500, status: "Paid" },
@@ -12,7 +12,7 @@ const data = [
     },
     {
         heading: "Room 2", name: "Vinod", mobile: "9873447012", record: [
-            { month: "Apr 2023", newUnits: "-", oldUnits: "2261", rentAmt: 3500, status: "Unpaid" },
+            { month: "Apr 2023", newUnits: "2332", oldUnits: "2261", rentAmt: 3500, status: "Unpaid" },
             { month: "Mar 2023", newUnits: "2261", oldUnits: "2238", rentAmt: 3500, status: "Paid" },
             // { month: "Feb 2023", newUnits: "2238", oldUnits: "2238", rentAmt: 3500, status: "Paid" },
             // { month: "Jan 2023", newUnits: "2238", oldUnits: "2238", rentAmt: 3500, status: "Paid" },
@@ -64,26 +64,26 @@ const data = [
         ]
     },
     {
-        heading: "Room 3", name: "Rahul", mobile: "", record: [
-            { month: "Apr 2023", newUnits: "?", oldUnits: "272", rentAmt: 3500, status: "Unpaid" },
-            { month: "Mar 2023", newUnits: "272", oldUnits: "231", rentAmt: 3500, status: "Unpaid" },
-            { month: "Feb 2023", newUnits: "231", oldUnits: "191", rentAmt: 3500, status: "Paid" },
+        heading: "Room 3", name: "Rahul", mobile: "8237369780", record: [
+            { month: "Apr 2023", newUnits: "301", oldUnits: "272", rentAmt: 2600, status: "Unpaid" },
+            { month: "Mar 2023", newUnits: "272", oldUnits: "231", rentAmt: 2600, status: "Paid" },
+            { month: "Feb 2023", newUnits: "231", oldUnits: "191", rentAmt: 2600, status: "Paid" },
         ]
     },
     {
         heading: "Room 4", name: "Vidya", mobile: "8700872374", record: [
-            { month: "18 Apr 2023", newUnits: "3990", oldUnits: "3939", rentAmt: 3500, status: "Unpaid" },
-            { month: "18 Mar 2023", newUnits: "3939", oldUnits: "3890", rentAmt: 3500, status: "Paid" },
-            // { month: "18 Feb 2023", newUnits: "3890", oldUnits: "3880", rentAmt: 3500, status: "Paid" },
-            // { month: "18 Jan 2023", newUnits: "3880", oldUnits: "start", rentAmt: 3500, status: "Paid" },
-            // { month: "18 Dec 2022", newUnits: "189", oldUnits: "177", rentAmt: 3500, status: "Paid" },
-            // { month: "18 Nov 2022", newUnits: "177", oldUnits: "135", rentAmt: 3500, status: "Paid" },
-            // { month: "18 Oct 2022", newUnits: "135", oldUnits: "start", rentAmt: 3500, status: "Paid" },
+            { month: "18 Apr 2023", newUnits: "3990", oldUnits: "3939", rentAmt: 3700, status: "Paid" },
+            { month: "18 Mar 2023", newUnits: "3939", oldUnits: "3890", rentAmt: 3700, status: "Paid" },
+            // { month: "18 Feb 2023", newUnits: "3890", oldUnits: "3880", rentAmt: 3700, status: "Paid" },
+            // { month: "18 Jan 2023", newUnits: "3880", oldUnits: "start", rentAmt: 3700, status: "Paid" },
+            // { month: "18 Dec 2022", newUnits: "189", oldUnits: "177", rentAmt: 3700, status: "Paid" },
+            // { month: "18 Nov 2022", newUnits: "177", oldUnits: "135", rentAmt: 3700, status: "Paid" },
+            // { month: "18 Oct 2022", newUnits: "135", oldUnits: "start", rentAmt: 3700, status: "Paid" },
         ]
     },
     {
         heading: "Room 5", name: "Rajender", mobile: "8527261971", record: [
-            { month: "Apr 2023", newUnits: "?", oldUnits: "4727", rentAmt: 3800, status: "Unpaid" },
+            { month: "Apr 2023", newUnits: "4763", oldUnits: "4727", rentAmt: 3800, status: "Paid" },
             { month: "Mar 2023", newUnits: "4727", oldUnits: "4706", rentAmt: 3800, status: "Paid" },
             { month: "Feb 2023", newUnits: "4706", oldUnits: "4682", rentAmt: 3800, status: "Paid" },
             { month: "Jan 2023", newUnits: "4682", oldUnits: "4649", rentAmt: 3800, status: "Paid" },
@@ -96,7 +96,17 @@ const data = [
     },
     {
         heading: "Room 6", name: "Keshav", mobile: "9352167689", record: [
-            { month: "-", newUnits: "0", oldUnits: "0", rentAmt: 4000, status: "-" },
+            { month: "May 2023", newUnits: "45", oldUnits: "45", rentAmt: 2000, status: "Paid" },
+            { month: "31 Apr 2023", newUnits: "45", oldUnits: "45", rentAmt: 1000, status: "Paid" },
+            { month: "13 Apr 2023", newUnits: "45", oldUnits: "16", rentAmt: 2000, status: "Paid" },
+            { month: "13 Mar 2023", newUnits: "16", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Feb 2023", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Jan 2023", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Dec 2022", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Nov 2022", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Oct 2022", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Sep 2022", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
+            { month: "13 Aug 2022", newUnits: "0", oldUnits: "0", rentAmt: 2000, status: "Paid" },
         ]
     },
     {
